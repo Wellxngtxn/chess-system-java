01fba916ad719b9af1f1f98eb003d3cea7e571f8
Wellington <wellingtoncerqueira17@gmail.com>
2024-07-16 19:42:29 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,27 @@
 package application;
 
 import chess.ChessMath;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        
-       ChessMath chessMatch = new ChessMath();
-       UI.printBoard(chessMatch.getPieces());
+       
+       Scanner leia = new Scanner(System.in);  
+       ChessMath chessMath = new ChessMath();
+       
+       while(true){
+           UI.printBoard(chessMath.getPieces());
+           System.out.println();
+           System.out.print("Source: ");
+           ChessPosition source = UI.readChessPosition(leia);
+           
+           System.out.println();
+           System.out.print("Target: ");
+           ChessPosition target = UI.readChessPosition(leia);
+           
+           ChessPiece capturedPiece = chessMath.performChessMove(source, target);
+       }  
     }
 }
